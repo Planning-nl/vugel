b1866c7e31262f9fe46349a419439cc1d9462b86
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-04-09 10:11:15 +0200

    fix(focus): ensure that canvas is focusable

--- src/events/focus/FocusManager.ts
+++ src/events/focus/FocusManager.ts
@@ -5,8 +5,15 @@ class FocusManager {
     private activeNode?: Node = undefined;
 
     constructor(private canvasElement: HTMLCanvasElement, private stage: Stage) {
+        this.ensureFocusable();
         this.canvasElement.addEventListener("focus", (e) => this.onFocus(e));
         this.canvasElement.addEventListener("blur", (e) => this.onBlur(e));
+    }
+
+    private ensureFocusable() {
+        if (!this.canvasElement.hasAttribute("tabindex")) {
+            this.canvasElement.setAttribute("tabindex", "-1");
+        }
     }
 
     private onFocus(e: FocusEvent) {
