c7d45700a969a31eeee8907e2ec01f73d62e3943
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-04-09 08:04:35 +0200

    feat(dashed properties): auto-convert dashed properties to camel case

--- src/runtime/patchProp.ts
+++ src/runtime/patchProp.ts
@@ -12,5 +12,29 @@ export function patchProp(
     parentSuspense?: SuspenseBoundary,
     unmountChildren?: any,
 ) {
+    key = convertDashedToCamelCase(key);
     patchElProp(el, key, nextValue, prevChildren, parentComponent, parentSuspense, unmountChildren);
+}
+
+function convertDashedToCamelCase(key: string) {
+    if (key.indexOf("-") !== -1) {
+        const parts = key.split("-");
+        let result = parts[0];
+        const n = parts.length;
+        let i = 1;
+        while (i < n) {
+            const part = parts[i];
+            const l = part.length;
+            if (l) {
+                result += part.charAt(0).toUpperCase();
+            }
+            if (l > 1) {
+                result += part.substr(1);
+            }
+            i++;
+        }
+        return result;
+    } else {
+        return key;
+    }
 }
