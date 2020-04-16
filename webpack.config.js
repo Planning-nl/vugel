82ba89b18b114014c719e4cc59ca0cc10085d1dd
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-04-16 12:49:02 +0200

    fix(bundle): webpack must bundle with compiler-core because Vugel uses some internals

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ module.exports = require("@planning.nl/webpack-config")({
     externals: {
         vue: { ...vueExternals },
         "@vue/runtime-core": { ...vueExternals },
-        "@vue/compiler-core": { ...vueExternals },
         "@vue/reactivity": { ...vueExternals },
     },
 });
