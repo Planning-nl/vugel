fef1bf81bd46115caa51d58dfbd558b00a3bc15e
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-04-28 07:57:20 +0200

    feat(shader/texture): simplify direct shader/texture tags: just provide a reference to the texture element (make sure not to use ref).

--- src/runtime/nodes/effects/Shader.ts
+++ src/runtime/nodes/effects/Shader.ts
@@ -1,17 +1,8 @@
 import { Container } from "../Container";
 import { Shader as Tree2dShader } from "tree2d";
-import { Constructor } from "../../utils/TypeUtils";
 
 export class Shader extends Container {
-    set type(v: Constructor<Tree2dShader>) {
-        this.el.renderToTexture = true;
-        this.el.shader = new v(this.stage.context);
-    }
-
-    set options(v: Record<string, any>) {
-        const keys = Object.keys(v);
-        keys.forEach((key: string) => {
-            (this.el.shader as any)[key] = v[key];
-        });
+    set shader(v: Tree2dShader) {
+        this.el.shader = v;
     }
 }
--- src/runtime/nodes/textures/Texture.ts
+++ src/runtime/nodes/textures/Texture.ts
@@ -1,16 +1,8 @@
 import { Node } from "../Node";
 import { Texture as Tree2dTexture } from "tree2d";
-import { Constructor } from "../../utils/TypeUtils";
 
 export class Texture extends Node {
-    set type(v: Constructor<Tree2dTexture>) {
-        this.el.texture = new v(this.stage);
-    }
-
-    set options(v: Record<string, any>) {
-        const keys = Object.keys(v);
-        keys.forEach((key: string) => {
-            (this.el.texture as any)[key] = v[key];
-        });
+    set texture(v: Tree2dTexture) {
+        this.el.texture = v;
     }
 }
