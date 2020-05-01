cd47a69178982b08e4ba378531e455607065277b
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-05-01 14:21:32 +0200

    feat(vugel): allow using a factory when creating shaders or textures

--- src/runtime/nodes/effects/Shader.ts
+++ src/runtime/nodes/effects/Shader.ts
@@ -1,8 +1,22 @@
 import { Container } from "../Container";
 import { Shader as Tree2dShader } from "tree2d";
+import { CoreContext } from "tree2d";
+
+export type ShaderFactory = (context: CoreContext) => Tree2dShader;
+
+const factories = new Map<ShaderFactory, Tree2dShader>();
 
 export class Shader extends Container {
     set shader(v: Tree2dShader) {
         this.el.shader = v;
+    }
+
+    set ["shader-factory"](v: ShaderFactory) {
+        let shader = factories.get(v);
+        if (!shader) {
+            shader = v(this.stage.context);
+            factories.set(v, shader);
+        }
+        this.shader = shader;
     }
 }
--- src/runtime/nodes/textures/Texture.ts
+++ src/runtime/nodes/textures/Texture.ts
@@ -1,8 +1,22 @@
 import { Node } from "../Node";
+import { VugelStage } from "../../../wrapper";
 import { Texture as Tree2dTexture } from "tree2d";
+
+export type TextureFactory = (stage: VugelStage) => Tree2dTexture;
+
+const factories = new Map<TextureFactory, Tree2dTexture>();
 
 export class Texture extends Node {
     set texture(v: Tree2dTexture) {
         this.el.texture = v;
+    }
+
+    set ["texture-factory"](v: TextureFactory) {
+        let texture = factories.get(v);
+        if (!texture) {
+            texture = v(this.stage);
+            factories.set(v, texture);
+        }
+        this.texture = texture;
     }
 }
