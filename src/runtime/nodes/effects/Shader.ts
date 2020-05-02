e2b3dd1013ea9dd47912cee34073d62755dc99c8
Bas van Meurs <bvanmeurs1985@gmail.com>
2020-05-02 20:55:12 +0200

    feat(texture): revert reusability; you can easily do that manually (see ParticlesExample)

--- src/runtime/nodes/effects/Shader.ts
+++ src/runtime/nodes/effects/Shader.ts
@@ -2,24 +2,14 @@ import { Container } from "../Container";
 import { Shader as Tree2dShader } from "tree2d";
 import { CoreContext } from "tree2d";
 
-export type ShaderFactory = (context: CoreContext) => { shader: Tree2dShader; reusable: boolean };
-
-const factories = new Map<ShaderFactory, Tree2dShader>();
+export type ShaderFactory = (context: CoreContext) => Tree2dShader | undefined;
 
 export class Shader extends Container {
-    set shader(v: Tree2dShader) {
+    set shader(v: Tree2dShader | undefined) {
         this.el.shader = v;
     }
 
     set ["shader-factory"](v: ShaderFactory) {
-        let shader = factories.get(v);
-        if (!shader) {
-            const info = v(this.stage.context);
-            shader = info.shader;
-            if (info.reusable) {
-                factories.set(v, shader);
-            }
-        }
-        this.shader = shader;
+        this.shader = v(this.stage.context);
     }
 }
--- src/runtime/nodes/textures/Texture.ts
+++ src/runtime/nodes/textures/Texture.ts
@@ -2,24 +2,14 @@ import { Node } from "../Node";
 import { VugelStage } from "../../../wrapper";
 import { Texture as Tree2dTexture } from "tree2d";
 
-export type TextureFactory = (stage: VugelStage) => { texture: Tree2dTexture; reusable: boolean };
-
-const factories = new Map<TextureFactory, Tree2dTexture>();
+export type TextureFactory = (stage: VugelStage) => Tree2dTexture | undefined;
 
 export class Texture extends Node {
-    set texture(v: Tree2dTexture) {
+    set texture(v: Tree2dTexture | undefined) {
         this.el.texture = v;
     }
 
     set ["texture-factory"](v: TextureFactory) {
-        let texture = factories.get(v);
-        if (!texture) {
-            const info = v(this.stage);
-            texture = info.texture;
-            if (info.reusable) {
-                factories.set(v, texture);
-            }
-        }
-        this.texture = texture;
+        this.texture = v(this.stage);
     }
 }
